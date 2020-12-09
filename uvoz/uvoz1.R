56d8df4a97ebbced5c918acbf455504e0a37884f
Pia Klemenc <pia.klemenc1@gmail.com>
2020-12-09 21:22:08 +0100

    ločitev stolpca na leta in mesece, grupiranje in izračun povprečne letne plače

--- uvoz/uvoz1.R
+++ uvoz/uvoz1.R
@@ -9,7 +9,27 @@ library(maptools)
 library(rgeos)
 library(rgdal)
 library(tibble)
+library(stringr)
 
-uvozi.place <- read_csv('podatki/place.csv', locale=locale(encoding="Windows-1250"), na=c('n','z','-','/'))
-colnames(uvozi.place) = c('STATISTICNA.REGIJA', 'SKD.DEJAVNOST', 'LETO.MESEC', 'NETO.MESECNA.PLACA')
+uvozi.place <- read_csv('podatki/place.csv', locale=locale(encoding="Windows-1250"), na=c('n','z','-','/')) %>%
+  separate(MESEC,  into = c('leto', 'mesec'), sep = 'M')
+  
+colnames(uvozi.place) = c('STATISTICNA.REGIJA', 'SKD.DEJAVNOST', 'LETO', 'MESEC', 'NETO.MESECNA.PLACA')
+
+uvozi.place <- uvozi.place %>%
+  group_by(STATISTICNA.REGIJA, SKD.DEJAVNOST, LETO) %>%
+  summarize(POVPRECNA.LETNA.PLACA = mean(NETO.MESECNA.PLACA, na.rm=TRUE))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
