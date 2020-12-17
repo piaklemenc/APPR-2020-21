17822105b99966d3f750b54289d4bcc0493cd5dd
Pia Klemenc <pia.klemenc1@gmail.com>
2020-12-17 09:21:20 +0100

    popravki in dopolnitve

--- lib/libraries.r
+++ lib/libraries.r
@@ -5,6 +5,26 @@ library(tidyr)
 library(tmap)
 library(shiny)
 
+#pobriši duplikate!
+library(dplyr)
+library(DT)
+library(digest)
+library(ggplot2)
+library(knitr)
+library(maptools)
+library(mosaic)
+library(nlme)
+library(rgeos)
+library(rgdal)
+library(rmarkdown)
+library(readr)
+library(stringr)
+library(shiny)
+library(tidyr)
+library(tibble)
+library(tmap)
+library(lubridate)
+
 options(gsubfn.engine="R")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
--- uvoz/uvoz1.R
+++ uvoz/uvoz1.R
@@ -1,35 +1,25 @@
-library(dplyr)
-library(DT)
-library(digest)
-library(ggplot2)
-library(knitr)
-library(maptools)
-library(mosaic)
-library(nlme)
-library(rgeos)
-library(rgdal)
-library(rmarkdown)
-library(readr)
-library(stringr)
-library(shiny)
-library(tidyr)
-library(tibble)
-library(tmap)
 
 
 
-uvozi.place <- read_csv('podatki/place.csv', locale=locale(encoding="Windows-1250"), na=c('n','z','-','/','-Inf', 'Inf')) %>%
-  separate(MESEC,  into = c('leto', 'mesec'), sep = 'M')
 
-colnames(uvozi.place) = c('STATISTICNA.REGIJA', 'SKD.DEJAVNOST', 'LETO', 'MESEC', 'NETO.MESECNA.PLACA')
-uvozi.place$LETO <- as.integer(uvozi.place$LETO)
-uvozi.place$NETO.MESECNA.PLACA <- as.numeric(uvozi.place$NETO.MESECNA.PLACA)
+uvozi.place <- read_csv('podatki/place.csv', locale=locale(encoding="Windows-1250"),
+                        na=c('n','z','-','/','-Inf', 'Inf'), skip=1,
+                        col_names=c('STATISTICNA.REGIJA', 'SKD.DEJAVNOST',
+                                    'MESEC', 'NETO.MESECNA.PLACA')) %>%
+  mutate(MESEC=parse_date(MESEC, "%YM%m"))
+#%>%
+#  separate(MESEC,  into = c('leto', 'mesec'), sep = 'M')
+
+#colnames(uvozi.place) = c('STATISTICNA.REGIJA', 'SKD.DEJAVNOST', 'LETO', 'MESEC', 'NETO.MESECNA.PLACA')
+#uvozi.place$LETO <- as.integer(uvozi.place$LETO)
+#uvozi.place$NETO.MESECNA.PLACA <- as.numeric(uvozi.place$NETO.MESECNA.PLACA)
 
 
 #povprecna letna placa v vsaki regiji za vsako dejavnost
-uvozi.place <- uvozi.place %>%
-  group_by(STATISTICNA.REGIJA, SKD.DEJAVNOST, LETO) %>%
-  summarize(POVPRECNA.LETNA.PLACA = mean(NETO.MESECNA.PLACA, na.rm=TRUE))
+letne.place <- uvozi.place %>%
+  group_by(STATISTICNA.REGIJA, SKD.DEJAVNOST, LETO=year(MESEC)) %>%
+  summarise(LETNA.PLACA=sum(NETO.MESECNA.PLACA)) %>%
+  summarize(POVPRECNA.LETNA.PLACA = mean(LETNA.PLACA, na.rm=TRUE))
 
 #max povprecna letna placa v vsaki regiji za vsako dejavnost
 place.max <- uvozi.place %>%
@@ -50,7 +40,7 @@ place.goriska.max <- place.max %>% filter(STATISTICNA.REGIJA == 'Goriška') %>%
   arrange(MAX.LETNA.PLACA)
 
 #zemljevid regij
-source("https://raw.githubusercontent.com/jaanos/APPR-2020-21/master/lib/uvozi.zemljevid.r")
+
 slo <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_SVN_shp.zip", "gadm36_SVN_1", encoding="UTF-8")
 tm_shape(slo) + tm_polygons("NAME_1")
 
@@ -58,7 +48,8 @@ tm_shape(slo) + tm_polygons("NAME_1")
 #risanje
 g1 <- ggplot(place.max) + aes(x=SKD.DEJAVNOST , y=MAX.LETNA.PLACA ) + geom_point()
 
-g2 <- ggplot(place.gorenjska.max) + aes(x=SKD.DEJAVNOST, y=MAX.LETNA.PLACA) + geom_point()
+g2 <- ggplot(place.gorenjska.max) + aes(x=substr(SKD.DEJAVNOST, 1, 1), y=MAX.LETNA.PLACA) + geom_col() +
+  xlab("Dejavnost")
 
 
 
