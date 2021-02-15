ff5f267f0a3fe2778914772cbbd40205cab5909c
Pia Klemenc <pia.klemenc1@gmail.com>
2021-02-15 23:25:14 +0100

    grafi

--- uvoz/uvoz1.R
+++ uvoz/uvoz1.R
@@ -28,12 +28,11 @@ place.min <- letne.place %>%
   summarize(MIN.LETNA.PLACA = min(POVPRECNA.LETNA.PLACA, na.rm=TRUE))
 
 
-#max povprecnih letnih plac posamezne regije
+#place na gorenjskem
 maxplace.gorenjska <- place.max %>% filter(STATISTICNA.REGIJA == 'Gorenjska') %>%
   arrange(MAX.LETNA.PLACA)  
   
-maxplace.goriska <- place.max %>% filter(STATISTICNA.REGIJA == 'Goriška') %>%
-  arrange(MAX.LETNA.PLACA)
+place.gorenjska <- uvozi.place %>%filter(STATISTICNA.REGIJA == 'Gorenjska')
 
 #zemljevid regij
 place.regije$STATISTICNA.REGIJA[10] <- "Notranjsko-kraška"
@@ -44,10 +43,44 @@ slo1 <- tm_shape(merge(slo, place.regije,  by.x="NAME_1", by.y="STATISTICNA.REGI
   tm_text('NAME_1', size = 0.7)
 
 #risanje
-g1 <- ggplot(place.max) + aes(x=SKD.DEJAVNOST , y=MAX.LETNA.PLACA ) + geom_point()
+g1 <- ggplot(place.max) + aes(x=substr(SKD.DEJAVNOST, 1, 1), y=MAX.LETNA.PLACA,fill="red") + geom_col() +
+  xlab("Dejavnost")
+  
 
 g2 <- ggplot(maxplace.gorenjska) + aes(x=substr(SKD.DEJAVNOST, 1, 1), y=MAX.LETNA.PLACA) + geom_col() +
   xlab("Dejavnost")
+
+#vsota povprečnih plač regij za vsako dejavnost
+g3 <- ggplot(letne.place) + aes(x=substr(SKD.DEJAVNOST, 1,1) , y=POVPRECNA.LETNA.PLACA, fill=STATISTICNA.REGIJA) +
+  geom_col(col = 'black') +
+  xlab("Dejavnost")
+
+#višina plač v regijah za vsako panogo
+g4 <- ggplot(letne.place) + aes(x=substr(SKD.DEJAVNOST, 1,1), y=POVPRECNA.LETNA.PLACA, fill=STATISTICNA.REGIJA) +
+  geom_col(position="dodge",col = 'black')
+
+#grafi po panogah
+g5 <- ggplot(data=letne.place, aes(x=STATISTICNA.REGIJA, y=POVPRECNA.LETNA.PLACA, fill=STATISTICNA.REGIJA)) +
+  geom_col() + facet_wrap(~ substr(SKD.DEJAVNOST, 1,1)) +
+  coord_flip() + theme(axis.text.x = element_text(size = 6, angle = 90, vjust = 0.5, hjust=1), legend.position="none")
+
+#grafi po regijah
+g6 <- ggplot(data=letne.place, aes(x=substr(SKD.DEJAVNOST,1,1), y=POVPRECNA.LETNA.PLACA, fill=substr(SKD.DEJAVNOST, 1,1))) +
+  geom_col() + facet_wrap(~ STATISTICNA.REGIJA) +
+  coord_flip() + theme(axis.text.x = element_text(size = 6, angle = 90, vjust = 0.5, hjust=1), legend.position="none") +
+  xlab("PANOGA")
+ 
+#grafi plač na gorenjskem za posamezno panogo
+#g7 <- ggplot(data=place.gorenjska, aes(x=MESEC, y=NETO.MESECNA.PLACA, fill=MESEC)) +
+ # geom_col() + facet_wrap(~ SKD.DEJAVNOST) + 
+  #theme(axis.text.x = element_text(size = 6, angle = 90, vjust = 0.5, hjust=1),legend.position="none" ) +
+  #xlab("LETO") + ylab("NETO PLAČA")
+
+#grafi plač na gorenjskem za posamezno panogo
+g7 <- ggplot(data=place.gorenjska, aes(x=MESEC, y=NETO.MESECNA.PLACA, col=SKD.DEJAVNOST)) +
+  geom_line() + facet_wrap(~SKD.DEJAVNOST) +
+  theme(axis.text.x = element_text(size = 6, angle = 90, vjust = 0.5, hjust=1),legend.position="none" ) +
+  xlab("LETO") + ylab("NETO PLAČA")
 
 
 
